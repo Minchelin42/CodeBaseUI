bf0984fc58c4a9b3dc6de862be91b9a08c006858
Minchelin42 <mje0211@naver.com>
2024-01-28 20:41:43 +0900

    DESIGN: ShoppingSearchView CodeBaseUI 구현

---
+++ CodeBaseUI/SearchListTableViewCell.swift
@@ -0,0 +1,83 @@
+//
+//  SearchListTableViewCell.swift
+//  CodeBaseUI
+//
+//  Created by 민지은 on 2024/01/28.
+//
+
+import UIKit
+import SnapKit
+
+class SearchListTableViewCell: UITableViewCell {
+    
+
+    let circleView = UIView()
+    let image = UIImageView()
+    let productLabel = UILabel()
+    let deleteButton = UIButton()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(circleView)
+        circleView.addSubview(image)
+        contentView.addSubview(productLabel)
+        contentView.addSubview(deleteButton)
+        
+        self.backgroundColor = .black
+        
+        circleView.backgroundColor = .clear
+        DispatchQueue.main.async {
+            self.circleView.clipsToBounds = true
+            self.circleView.layer.cornerRadius = self.circleView.frame.width / 2
+        }
+        circleView.layer.borderWidth = 1
+        circleView.layer.borderColor = UIColor.gray.cgColor
+        
+        image.image = UIImage(systemName: "magnifyingglass")
+        image.tintColor = .white
+        image.backgroundColor = .clear
+        
+        productLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        productLabel.textColor = .white
+        productLabel.backgroundColor = .clear
+        
+        deleteButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        deleteButton.tintColor = .gray
+        deleteButton.backgroundColor = .clear
+        
+        circleView.snp.makeConstraints { make in
+            make.size.equalTo(35)
+            
+            make.leading.equalTo(8)
+            make.centerY.equalToSuperview()
+        }
+        
+        image.snp.makeConstraints { make in
+            make.size.equalTo(20)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        productLabel.snp.makeConstraints { make in
+            make.leading.equalTo(circleView.snp.trailing).offset(15)
+            make.trailing.greaterThanOrEqualTo(10)
+            make.centerY.equalToSuperview()
+        }
+        
+        deleteButton.snp.makeConstraints { make in
+            make.size.equalTo(35)
+            make.centerY.equalToSuperview()
+            make.trailing.equalTo(8)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+#Preview {
+    ShoppingSearchViewController()
+}
---
+++ CodeBaseUI/ShoppingSearchViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ShoppingSearchViewController.swift
+//  CodeBaseUI
+//
+//  Created by 민지은 on 2024/01/28.
+//
+
+import UIKit
+import SnapKit
+
+class ShoppingSearchViewController: UIViewController {
+    
+    let list = ["맥북 거치대", "레오폴드 저소움 적축", "나이키 러닝화", "아이폰 15 256", "뉴발란스", "카드지갑"]
+
+    let searchBar = UISearchBar()
+    let topView = UIView()
+    let searchLabel = UILabel()
+    let clearButton = UIButton()
+    
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .black
+        
+        view.addSubview(searchBar)
+        view.addSubview(topView)
+        topView.addSubview(searchLabel)
+        topView.addSubview(clearButton)
+        view.addSubview(tableView)
+        
+        searchBar.barTintColor = .black
+        searchBar.searchTextField.backgroundColor = .darkGray
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        
+        searchBar.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        topView.backgroundColor = .black
+        
+        topView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(40)
+            
+            make.top.equalTo(searchBar.snp.bottom)
+        }
+        
+        searchLabel.text = "최근 검색"
+        searchLabel.font = .boldSystemFont(ofSize: 15)
+        searchLabel.textColor = .white
+        
+        searchLabel.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(10)
+        }
+        
+        clearButton.setTitle("모두 지우기", for: .normal)
+        clearButton.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        clearButton.setTitleColor(.green, for: .normal)
+        
+        clearButton.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.centerY.equalToSuperview()
+            
+            make.trailing.equalTo(10)
+        }
+        
+        tableView.backgroundColor = .black
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(topView.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(SearchListTableViewCell.self, forCellReuseIdentifier: "SearchListTableViewCell")
+
+    }
+
+}
+
+extension ShoppingSearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchListTableViewCell", for: indexPath) as! SearchListTableViewCell
+        cell.productLabel.text = list[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
+}
+
+#Preview {
+    ShoppingSearchViewController()
+}
